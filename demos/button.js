7723297914f6e2e4ecc39b5fcb098d2c5be0ad59
lixianjing <xianjimli@hotmail.com>
2019-07-11 00:34:56 -0700

    update demos

--- demos/button.js
+++ demos/button.js
@@ -7,11 +7,13 @@ function applicationInit() {
 
   ok.on(TEventType.CLICK, function(evt) {
     var e = TPointerEvent.cast(evt);
+    console.log("global: " + TGlobal.getPointerX() + " " + TGlobal.getPointerY() + " " + TGlobal.isPointerPressed());
     console.log("on click: " + e.x + " " + e.y);
-    console.log("global: " + global.get_pointer_x() + " " + global.get_pointer_y() + " " + global.is_pointer_pressed());
 
     return TRet.OK;
   });
+  
+  console.log("global: " + TGlobal.getPointerX() + " " + TGlobal.getPointerY() + " " + TGlobal.isPointerPressed());
 
   win.layout();
 }
