e4b39956f2d67a49513edb133972884cfd1a6abe
xianjimli <xianjimli@gmail.com>
2020-10-12 13:34:37 +0800

    sync

--- src/c/tk_jerryscript.c
+++ src/c/tk_jerryscript.c
@@ -8884,7 +8884,7 @@ jsvalue_t wrap_widget_set_prop_pointer(const jerry_value_t func_obj_val, const j
     ret_t ret = (ret_t)0;
     widget_t* widget = (widget_t*)jsvalue_get_pointer(ctx, argv[0], "widget_t*");
     const char* name = (const char*)jsvalue_get_utf8_string(ctx, argv[1]);
-    void** v = (void**)jsvalue_get_pointer(ctx, argv[2], "void**");
+    void* v = (void*)jsvalue_get_pointer(ctx, argv[2], "void*");
     ret = (ret_t)widget_set_prop_pointer(widget, name, v);
     TKMEM_FREE(name);
 
